979666cdf17335cdf662304a915855d26de17cf3
vancado <tech-c@vancado-service.de>
2021-02-22 12:58:42 +0100

    initial commit

---
+++ Test Suites/Vancado Kontakt-Formular.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Vancado Kontakt-Formular</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d7013384-f86f-40c4-bfb5-2e3c14ac8e16</testSuiteGuid>
+   <testCaseLink>
+      <guid>40e90170-0457-4bb8-8e45-4d24045b6e2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Base test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
